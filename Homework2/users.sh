28945e65175e4cfb3ace4d8e91aa75653056f8b1
root <root@localhost.localdomain>
2022-01-25 11:00:47 +0200

    add HW2

--- Homework2/users.sh
+++ Homework2/users.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-read name
-read pref
+read -p "Enter Name" name
+read -p "Enter prefix" pref
 
 count=0
 while [ $count -lt $pref ]
   do
     (( count++ ))
     echo $count
-    useradd -p $name$count "$name$count"
+    useradd -m -p "$name$count" $name$count
+    chmod 666 /home/$name$count 
+   # chmod 700 /home/$name$count
   done
